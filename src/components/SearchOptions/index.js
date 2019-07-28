fc4249efb17922c056e51a73d8148e501b5e64f4
megashrieks <shrikanthbudya@gmail.com>
2019-07-28 17:55:29 +0530

    added style for search options component

--- src/components/SearchOptions/index.js
+++ src/components/SearchOptions/index.js
@@ -1,4 +1,16 @@
 import React from 'react';
+import './index.css';
+
 export default () => {
-    return <div>Search Options</div>
+    return <div className="search-options">
+        <div className="main-options">
+            <input className="classic-input" placeholder="book title ex:Harry Potter" />
+            <button className="search">
+                <i className = "fa fa-search"/>
+            </button>
+            <button className="search-options-toggle">
+                <i className = "fa fa-caret-down"/>
+            </button>
+        </div>
+    </div>
 }
